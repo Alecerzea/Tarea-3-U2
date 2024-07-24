588be59067332c5cbdd6473948bd2210edd85bb2
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2024-07-24 17:49:08 -0500

    main

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Users from './components/Users';
+import Students from './components/Students';
+import Teachers from './components/Teachers';
+import Courses from './components/Courses';
 
 function App() {
     return (
         <Router>
-            <Switch>
-                <Route path="/users" component={Users}
+            <div className="App">
+                <Switch>
+                    <Route path="/users" component={Users} />
+                    <Route path="/students" component={Students} />
+                    <Route path="/teachers" component={Teachers} />
+                    <Route path="/courses" component={Courses} />
+                </Switch>
+            </div>
+        </Router>
+    );
+}
+
+export default App;
