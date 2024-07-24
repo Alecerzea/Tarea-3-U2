dce736f3a3c44b57ba128328db4e0b965411efc2
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2024-07-24 17:32:15 -0500

    main

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Users from './components/Users';
-
-function App() {
-    return (
-        <Router>
-            <Switch>
-                <Route path="/users" component={Users}
--- src/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-    baseURL: 'http://localhost:8080/api/', // Assuming your Spring Boot server runs on port 8080
-});
-
-export default api;
